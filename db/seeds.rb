e69b1646cefca9c16830877e9bdbd358fe9c8230
“Vishnu <vichucoder96@gmail.com>
2018-11-14 22:52:51 -0600

    made some chanegs on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#Deposites:
+
+@coin = Coin.new(name:'quarter',value:25)
+@coin.save
+@coin = Coin.new(name:'penny',value:1)
+@coin.save
+@coin = Coin.new(name:'nickle',value:5)
+@coin.save
+@coin = Coin.new(name:'dime',value:10)
+@coin.save
+@coin = Coin.new(name:'quarter',value:25)
+@coin.save
+@coin = Coin.new(name:'quarter',value:25)
+@coin.save
+@coin = Coin.new(name:'dime',value:10)
+@coin.save
+@coin = Coin.new(name:'penny',value:1)
+@coin.save
+@coin = Coin.new(name:'dime',value:10)
+@coin.save
+@coin = Coin.new(name:'dime',value:10)
+@coin.save
+@coin = Coin.new(name:'nickle',value:5)
+@coin.save
+
+
+
+
+#Withdrawls:
+
+@draw = Draw.new(name:'quarter',value:25)
+@draw.save
+@draw = Draw.new(name:'dime',value:10)
+@draw.save
+@draw = Draw.new(name:'nickle',value:5)
+@draw.save
+@draw = Draw.new(name:'quarter',value:25)
+@draw.save
+@draw = Draw.new(name:'dime',value:10)
+@draw.save
+@draw = Draw.new(name:'penny',value:1)
+@draw.save
+@draw = Draw.new(name:'penny',value:1)
+@draw.save
+
+
+
+
+
+#Users: 
+
+@user = User.new(name:'Vishnu',email:'codervichu96@gmail.com', password:'password',password_confirmation:'password')
+@user.save
+@user = User.new(name:'Peter', email:'vichucoder96@gmail.com',password:'password',password_confirmation:'password')
+@user.save
